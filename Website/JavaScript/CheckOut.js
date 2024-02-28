7b6fc19498b3acee20545534a6df72845a7c64b1
Eleanor <57181053+Exator921@users.noreply.github.com>
2024-02-28 18:07:57 +0000

    Added the transparent background items

--- Website/JavaScript/CheckOut.js
+++ Website/JavaScript/CheckOut.js
@@ -1,2 +1,4 @@
-// Get the cupAndCoffee ID from HTML and declare it the cupAndCoffee variable
-const cupAndCoffee = document.getElementById("cupAndCoffee");
+// Get the Espresso and Table ID's from HTML and declare them as cup and table variable's.
+const cup = document.getElementById("Espresso_On_Table");
+const table = docment.getElementById("Table");
+
